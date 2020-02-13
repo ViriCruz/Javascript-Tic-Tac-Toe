dfa40d749cc87e2f49d32114f2d5753c86565ebf
viricruz <viridiana.cruz.olivera@gmail.com>
2020-02-13 15:26:28 -0700

    Add handleTurn, checkWinner, reset functionality. Also add variables module.

--- src/main.js
+++ src/main.js
@@ -51,7 +51,7 @@ const Player = (name, symbol) => {
 //     const player2 = Player('2', "0")
 // }
 const variables = (function() {
-    const cells = document.querySelectorAll(".square")
+    const cells = Array.from(document.querySelectorAll(".square"))
     const winningCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -65,20 +65,47 @@ const variables = (function() {
     const board = Array.from(Array(9).keys())
     const player1 = Player('1', "X")
     const player2 = Player('2', "0")
+    const messageTitle = document.querySelector('h2')
     return {
       cells,
       winningCombos,
       board,
       player1,
-      player2
+      player2,
+      messageTitle
     };
 }());
 
 let turn = variables.player1.getSymbol()
 
+
 const handleTurn = (event) => {
+  let index = variables.cells.findIndex((cell) => cell === event.target)
+  variables.board[index] = turn
   event.target.textContent = turn
   turn = turn === 'X' ? variables.player2.getSymbol() : variables.player1.getSymbol()
+  variables.messageTitle.textContent = `It's ${turn}'s turn`
+  checkWinner()
+}
+
+const checkWinner = () => {
+  let winner = ''
+  variables.winningCombos.forEach((item, i) => {
+    if(variables.board[item[0]] === variables.board[item[1]] && variables.board[item[1]] === variables.board[item[2]]) {
+      document.getElementById('board').removeEventListener('click', handleTurn)
+      winner = variables.board[item[0]]
+      variables.messageTitle.textContent = `Player ${winner} won!`
+      return
+    } else if(variables.board.every(function(element) { return typeof element === 'string'; })) {
+      variables.messageTitle.textContent = `It's a ${winner}`
+      winner = 'Tie!'
+    }
+  });
+  console.log(winner)
+  return winner
 }
 
 document.getElementById('board').addEventListener('click', handleTurn)
+document.getElementById('reset-button').addEventListener('click', () => {
+  location.reload()
+})
